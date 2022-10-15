19611d146a1d14c738a188a89de28dac0ece10fd
megoshmego <108840586+megoshmego@users.noreply.github.com>
2022-10-15 08:51:44 -0400

    Add files via upload

---
+++ best groccery/script.js
@@ -0,0 +1,67 @@
+
+    window.onload = loadTasks;
+
+    document.querySelector("form").addEventListener("submit", e => {
+      e.preventDefault();
+      addTask();
+    });
+
+    function loadTasks() {
+      if (localStorage.getItem("tasks") == null) return;
+        let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));    
+    tasks.forEach(task => {
+        const list = document.querySelector("ul");
+        const li = document.createElement("li");
+        li.innerHTML = `<input type="checkbox" onclick="taskComplete(this)" class="check" ${task.completed ? 'checked' : ''}>
+          <input type="text" value="${task.task}" class="task ${task.completed ? 'completed' : ''}" onfocus="getCurrentTask(this)" onblur="editTask(this)">
+          <i class="fa fa-trash" onclick="removeTask(this)"></i>`;
+        list.insertBefore(li, list.children[0]);
+      });
+    }
+
+    function addTask() {
+      const task = document.querySelector("form input");
+      const list = document.querySelector("ul");
+      
+      if (task.value === "") {
+        alert("No grocceries today?");
+        return false;
+      }
+      localStorage.setItem("tasks", JSON.stringify([...JSON.parse(localStorage.getItem("tasks") || "[]"), { task: task.value, completed: false }]));
+
+      // make items in list, checkbox, user input
+      const li = document.createElement("li");
+      li.innerHTML = `<input type="checkbox" onclick="taskComplete(this)" class="check">
+      <input type="text" value="${task.value}" class="task" onfocus="getCurrentTask(this)" onblur="editTask(this)">
+      <i class="fa-regular fa-trash-can" onclick="removeTask(this)"></i>`;
+      list.insertBefore(li, list.children[0]);
+      task.value = "";
+    }
+    
+    function taskComplete(event) {
+      let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
+      tasks.forEach(task => {
+        if (task.task === event.nextElementSibling.value) {
+          task.completed = !task.completed;
+        }
+      });
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+      event.nextElementSibling.classList.toggle("completed");
+    }
+    
+    function removeTask(event) {
+      let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
+      tasks.forEach(task => {
+        if (task.task === event.parentNode.children[1].value) {
+          tasks.splice(tasks.indexOf(task), 1);
+        }
+      });
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+      event.parentElement.remove();
+    }
+    var currentTask = null;
+    function getCurrentTask(event) {
+      currentTask = event.value;
+    }
+
+
